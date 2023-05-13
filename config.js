a4cee98ae6ae28e1a434485e5953977fe80700dd
kdbot1234 <122373604+kdbot1234@users.noreply.github.com>
2023-05-13 10:40:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94768873231', 'Kalana', true],
+  ['94768873231', 'Kalana', true], 
   [''] 
 ] //Number of owners
 
